af7ad54302582ad83aed48233e39e77e0a952695
lucassantossfc <lg8352089@gmail.com>
2021-03-09 16:15:07 -0300

    First commit

---
+++ src/regras.js
@@ -0,0 +1,22 @@
+const regras = (prefix, sender) => {
+	return `𝙱𝙴𝙼 𝚅𝙸𝙽𝙳𝙾𝚂 𝙰𝙾 𝙲𝙷𝙰𝚃𝚉𝙸𝙽 𝙳𝙾 𝚉𝙴𝚄𝚂📍
+𝑨𝒒𝒖𝒊 𝒆𝒔𝒕𝒂́ 𝒂𝒍𝒈𝒖𝒎𝒂𝒔 𝒓𝒆𝒈𝒓𝒂𝒔
+
+
+🀄📢Regras do grupo📢🀄
+✅Respeitar todos os shinobis.
+✅O foco do grupo é expressar seu amor por Naruto/Boruto.
+✅Compartilhar fotos/vídeos/informações sobre Naruto/Boruto.
+✅Spoiler só antecedendo "⚠aviso de spoiler⚠".
+🚫Links de outros grupos e correntes.
+🚫 Bullying, racismo, homofobia ou qualquer preconceito e ofensa que deixe outro shinobi desconfortável.
+🚫Conteúdo pornográfico ou ilegal.
+🚫 Divulgação de perfis( se for artístico somente com a permissão dos ADMs).
+⚠sujeito a BAN se desrespeitar as regras⚠
+
+
+
+❕ Quaisquer reclamações ou falhas chame no pv do administrador :)`
+}
+
+exports.regras = regras
